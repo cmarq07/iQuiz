1fdaed80545cdb18669c2cb27b5da6b7e4851f00
cmarq07 <christiancalloway17@gmail.com>
2022-05-12 16:30:12 -0700

    Assignment setup

---
+++ 7-iQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  7-iQuiz
+//
+//  Created by Christian Marquis Calloway on 5/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
